7404ac56464addc99c31803ad780327b18869a30
Sara Pitt <sara@mightily.com>
2021-02-15 10:38:14 -0500

    working on integrating the backend

--- src/js/modules/titles.js
+++ src/js/modules/titles.js
@@ -7,20 +7,21 @@ export default function titles() {
 
 	// ******************* Titles Animation ****************
 	const titleAnimations = [];
-	const titles = document.querySelectorAll( 'section:not(".hero.style-1") h1.title.animate-title span.line' ); // Grab the titles set to animate
+	const titles = document.querySelectorAll( 'section:not(.hero):not(.style-1) .title.animate-title span.line' ); // Grab the titles set to animate
 
 	titles.forEach((title, index) => { // Loop through titles
 		title.innerHTML = title.textContent.replace( /\S/g, `<span class='title-${index}-letter'>$&</span>`); // split each letter into it's own span
 		var section = title.closest('section'); // Grab the sections that contain the titles
+		console.log(section);
 		titleAnimations[index] = gsap.fromTo(`.title-${index}-letter`, { y: '-2em'}, {
-			// duration: 2,
 			stagger: 0.1,
 			y: 0,
 			scrollTrigger: {
 				trigger: section,
 				start: 'top 50%',
 			}
-		})
+		});
+		
 	});
 	
 }
